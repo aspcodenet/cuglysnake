b89e1bd2a72f3e89c45119a752d6f96fb1b2e5bc
aspcodenet <stefan.holmberg@systementor.se>
2023-11-14 08:49:00 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// TISDAG Kodartisdag !
+
+// Syfte: kodvana 
+
+// Men först: gå igegnom multidimensionella arrayer
+// genom ett exempel - en ETCH-A-SCETCH som kan bli en 
+// SNAKE om ni vill
+
+// VI SKA SKAPA ETCH A SCETCH IDAG
+// https://wokwi.com/projects/378100012183685121
+// fast vi bygger i console
+// som ni kan bygga vidare till SNAKE
+
+// Endast egen programmering sen
+
